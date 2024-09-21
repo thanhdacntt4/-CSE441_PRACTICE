e0334ae8aa98a3349955dcbfc16a531499a56905
thanhdacntt4 <teampkuminh@gmail.com>
2024-09-21 17:50:01 +0700

    ex01

---
+++ ex01/app/src/main/java/com/example/ex01/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.ex01;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    // Khai báo các biến giao diện tại đây
+    EditText edtA, edtB, edtKQ;
+    Button btncong;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Ánh xạ Id cho các biến giao diện
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        edtKQ = findViewById(R.id.edtKQ);
+        btncong = findViewById(R.id.btntong);
+
+        // Xử lý tương tác với người dùng
+        btncong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    // Lấy giá trị từ EditText và chuyển thành số nguyên
+                    int a = Integer.parseInt(edtA.getText().toString());
+                    int b = Integer.parseInt(edtB.getText().toString());
+
+                    // Tính tổng
+                    int kq = a + b;
+
+                    // Hiển thị kết quả lên EditText edtKQ
+                    edtKQ.setText(String.valueOf(kq));
+                } catch (NumberFormatException e) {
+                    edtKQ.setText("Lỗi nhập liệu");
+                }
+            }
+        });
+    }
+}
